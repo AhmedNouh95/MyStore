f23709080294c8ab488a945e5c02f34ed04c6e0d
Ahmed nouh <ahmednouh1575@gmail.com>
2023-03-09 03:15:42 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mystore/fragments/loginregister/IntroductionFragment.kt
@@ -0,0 +1,7 @@
+package com.example.mystore.fragments.loginregister
+
+import androidx.fragment.app.Fragment
+import com.example.mystore.R
+
+class IntroductionFragment : Fragment(R.layout.activity_login_register) {
+}
